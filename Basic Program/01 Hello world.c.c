603f154d8d771550ee27ed711501c16f88e8da5c
raviyadav5618 <109100826+raviyadav5618@users.noreply.github.com>
2022-09-25 16:25:10 +0530

    First Program C

---
+++ Basic Program/01 Hello world.c.c
@@ -0,0 +1,20 @@
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+
+{      
+       printf("\n\n\t_______!!!_______\t");
+        
+       printf("\n\n\tRaviraj Yadav\t ");
+     
+       printf("\n\n\t_______!!!_______\t");
+       
+       
+       getch();
+       return 0; 
+       
+       
+  }     
