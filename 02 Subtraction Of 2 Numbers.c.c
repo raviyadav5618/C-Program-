8dfe01bae5757f3bb99d94aeee104d0231149dcb
raviyadav5618 <109100826+raviyadav5618@users.noreply.github.com>
2022-10-04 12:26:22 +0530

    Add files via upload

---
+++ 02 Subtraction Of 2 Numbers.c.c
@@ -0,0 +1,20 @@
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{    
+  
+     int no1=55,no2=33,sum=0;
+     
+     sum=no1-no2;
+     
+     printf("\n\n subtraction Of %d-%d=%d.",no1,no2,sum);
+     
+     
+     getch();
+     return 0;
+     
+     
+  }
