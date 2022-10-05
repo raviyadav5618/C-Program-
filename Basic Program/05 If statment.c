b881eaa17d9ea9f20a5913021bbbc545856bbab9
raviyadav5618 <109100826+raviyadav5618@users.noreply.github.com>
2022-10-05 14:07:39 +0530

    Add files via upload

---
+++ Basic Program/05 If statment.c
@@ -0,0 +1,22 @@
+//Compiler version gcc  6.3.
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{    
+  
+    float Amount =0.0;
+    printf("\n Entar Your Account Balance= ");
+    scanf("%f",&Amount);
+    
+    
+     if(Amount>1000)
+      
+      printf(" \n\tWelcome To Mall !!!");
+      printf("\n\t Enjoy Your Shopping !!!");
+      
+       
+       getch();
+       return 0;
+       
+   }
