9136819c81050947fe2c7824c8a950a441b0a56d
raviyadav5618 <109100826+raviyadav5618@users.noreply.github.com>
2022-10-05 14:08:45 +0530

    Add files via upload

---
+++ Basic Program/06 if else.c
@@ -0,0 +1,29 @@
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+ 
+     int Age=0;
+    
+    
+     printf(" Enter Your Age");
+     scanf("%d",&Age);
+     
+     
+     if(Age>18)
+     {
+        printf(" Eligible For Voting !!!");
+     }   
+     
+     else
+     {
+       printf(" Not Eligible For Voting!!!");
+     }     
+     
+       getch();
+       return 0;
+  
+ }   
+     
