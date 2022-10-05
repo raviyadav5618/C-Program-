c3898e10c472cff31f4d1d59aaa6c44aad43e3c0
raviyadav5618 <109100826+raviyadav5618@users.noreply.github.com>
2022-10-05 14:06:27 +0530

    Add files via upload

---
+++ Basic Program/04 Multiplication Of Two Numbers.c
@@ -0,0 +1,25 @@
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{ 
+       
+       float No1=0.0,No2=0.0,sum=0.0;
+       
+        
+        printf("\n Entar Two Numbers=");
+        scanf("%f%f",&No1,&No2);
+        
+        
+         sum=No1*No2;
+         
+         printf(" Multiplication Of %f*%f=%f.",No1,No2,sum);
+         
+         
+         getch();
+         return 0;
+         
+         
+   }
+         
